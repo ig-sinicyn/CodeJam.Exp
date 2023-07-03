fc7a9ede196c7f8bc5def838151316f60512d02e
ig-sinicyn <igor.sinicyn@gmail.com>
2023-07-03 21:09:22 +0300

    Cleanup

--- CodeJam.Light.Tests/TargetingTests.cs
+++ CodeJam.Light.Tests/TargetingTests.cs
@@ -1,24 +1,39 @@
+using System.Threading.Tasks;
 
-
-#if NET45_OR_GREATER || TARGETS_NETCOREAPP
-using TaskEx = System.Threading.Tasks.Task;
-#elif NET40_OR_GREATER
-using TaskEx = System.Threading.Tasks.TaskEx;
-#else
-using TaskEx = System.Threading.Tasks.Task;
-
-#endif
-using FluentAssertions;
-
-// ReSharper disable once CheckNamespace
-// ReSharper disable MethodSupportsCancellation
-
-
-namespace CodeJam.Light.Tests;
+namespace CodeJam.Tests;
 
 [TestFixture]
 public class TargetingTests
 {
+	[UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
+	internal class CSharp10Features
+	{
+		[UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
+		public record Record(string Value)
+		{
+			public string? InitValue { get; init; }
+		}
+
+		[Pure, ContractsPure]
+		public async Task<Record?> TaskAsync()
+		{
+			await Task.Yield();
+			return new("Hello!");
+		}
+
+		public async ValueTask<string?> ValueTaskAsync()
+		{
+			await Task.Yield();
+			return "Hello!";
+		}
+
+		public async IAsyncEnumerable<string?> EnumerateAsync()
+		{
+			await Task.Yield();
+			yield return "Hello!";
+		}
+	}
+
 	[Test]
 	public void TestTasks()
 	{
@@ -34,5 +49,33 @@ public class TargetingTests
 		var a = (a: 1, b: 2, c: 3);
 
 		a.b.Should().Be(2);
+	}
+
+	[Test]
+	public void TestRecord()
+	{
+		var record = new CSharp10Features.Record("sample") { InitValue = "sample2" };
+
+		record.Value.Should().Be("sample");
+		record.InitValue.Should().Be("sample2");
+	}
+
+	[Test]
+	public async Task TestAsync()
+	{
+		var sample = new CSharp10Features();
+		(await sample.TaskAsync()).Should().Be("Hello!");
+		(await sample.ValueTaskAsync()).Should().Be("Hello!");
+	}
+
+	[Test]
+	public async Task TestAsyncEnumerable()
+	{
+		var sample = new CSharp10Features();
+
+		await foreach (var s in sample.EnumerateAsync())
+		{
+			s.Should().Be("Hello!");
+		}
 	}
 }
--- CodeJam.Light.Tests/Usings.cs
+++ CodeJam.Light.Tests/Usings.cs
@@ -2,4 +2,18 @@ global using global::System;
 global using global::System.Collections.Generic;
 global using global::System.Linq;
 
+global using JetBrains.Annotations;
+
+global using FluentAssertions;
+
 global using NUnit.Framework;
+
+global using ContractsPureAttribute = System.Diagnostics.Contracts.PureAttribute;
+
+#if NET45_OR_GREATER || TARGETS_NETCOREAPP
+global using TaskEx = System.Threading.Tasks.Task;
+#elif NET40_OR_GREATER
+global using TaskEx = System.Threading.Tasks.TaskEx;
+#else
+global using TaskEx = System.Threading.Tasks.Task;
+#endif
