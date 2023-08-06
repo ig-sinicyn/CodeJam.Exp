c0df79aaec7bbd61aedf285eca7a4c40e0c26e8f
ig-sinicyn <igor.sinicyn@gmail.com>
2023-08-06 17:48:25 +0300

    Rename assemblies in test results

--- tests.ps1
+++ tests.ps1
@@ -1,17 +1,26 @@
 # Run .Net Framework tests
-$testDlls = ls -r *.artifacts\*\Debug\*.Tests.dll | ? FullName -match "\\net\d+\\" `
+$logFilePath = '.artifacts\nunit-netfw-results.xml'
+$testDlls = ls -r '.artifacts\*\Debug\*.Tests.dll' | ? FullName -match '\\net\d+\\' `
   | % FullName | sort-object -Unique
-nunit3-console $testDlls --result=".artifacts\nunit-netfw-results.xml"
+nunit3-console $testDlls --result=$logFilePath
+
+## replace assemply name in tests
+$matchPattern = 'name="(?''name''.*?)\.dll" (?''rest''fullname=".*?\\(?''fw''net[^\\]*)\\[^\\]*?\.dll")'
+$replacement = 'name="${name} (${fw}).dll" ${rest}'
+cat -path $logFilePath | ??? -Replace $matchPattern, $replacement | > $logFilePath
+
 
 # Run .Net Core tests
-$testDlls = ls -r *.artifacts\*\Debug\*.Tests.dll | ? FullName -notmatch "\\net\d+\\" `
+$testDlls = ls -r '.artifacts\*\Debug\*.Tests.dll' | ? FullName -notmatch '\\net\d+\\' `
   | % FullName | sort-object -Unique
-dotnet vstest $testDlls  --logger "trx;LogFileName=..\.artifacts\nunit-netcore-results.xml"
+dotnet vstest $testDlls --results-directory '.\.artifacts\' --logger 'trx;LogFileName=nunit-netcore-results.xml'
+
+### TODO: replace assembly name in TRX files
 
 # Upload files
 $wc = New-Object 'System.Net.WebClient'
 $testResults = ls *.artifacts\nunit-*.xml | % FullName | sort-object -Unique
 foreach ($testResult in $testResults) {
-  echo "UploadFile: https://ci.appveyor.com/api/testresults/nunit3/$env:APPVEYOR_JOB_ID from $testResult"
-  $wc.UploadFile("https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)", $testResult)  
+  echo 'UploadFile: https://ci.appveyor.com/api/testresults/nunit3/$env:APPVEYOR_JOB_ID from $testResult'
+  $wc.UploadFile('https://ci.appveyor.com/api/testresults/nunit3/$($env:APPVEYOR_JOB_ID)', $testResult)  
 }
