15c4d05a7ccb9e4f872e28c017ed5d1fc4343949
ig-sinicyn <igor.sinicyn@gmail.com>
2023-08-08 22:26:01 +0300

    Fix pipeline

--- tests.ps1
+++ tests.ps1
@@ -1,25 +1,47 @@
-# Run .Net Framework tests
-$logFilePath = '.artifacts\nunit-netfw-results.xml'
-$testDlls = ls -r '.artifacts\*\Debug\*.Tests.dll' | ? FullName -match '\\net\d+\\' `
-  | % FullName | Sort-Object -Unique
-nunit3-console $testDlls --result=$logFilePath
+<#
+  Feel my pain. Here's incomplete list of issues that prevents us just to use existing tooling:
+  * https://github.com/appveyor/ci/issues/1894 - root cause. Test results are overwritten
+  * https://github.com/nunit/nunit/issues/3790 - bonus issue: no support for legacy frameforks on dotnet test
+  * https://github.com/microsoft/vstest/issues/4355 - dotnet vstest does not emits TFM for the trx file
+  * https://github.com/microsoft/vstest/issues/880 - dotnet test merges test result for multitargeting test
+  * https://github.com/nunit/nunit/pull/2724 - PR for appveyor format support (abandoned)
+  *
+#>
 
-## replace assemply name in tests
-$matchPattern = 'name="(?''name''.*?)\.dll" fullname="(?''fullname''.*?\\(?''fw''net[^\\]*)\\[^\\]*?)\.dll"'
-$replacement = 'name="${name} (${fw}).dll" fullname="${fullname} (${fw}).dll"'
+
+# Run .Net Framework tests using nunit3-console
+## We use nunit-console here as as dotnet test does not support legacy net frameworks
+$testNetFwDlls = ls -r '.artifacts\*\Debug\*.Tests.dll' | ? FullName -match '\\net\d+\\' ` | % FullName
+$logFilePath = '.artifacts\nunit_netframework.xml'
+nunit3-console $testNetFwDlls --result=$logFilePath
+
+## Update test report: add TFM to the assemply name
+$matchPattern = 'name="(?''name''.*?\.dll)" fullname="(?''fullname''.*?\\(?''tfm''net[^\\]*)\\[^\\]*?\.dll)"'
+$replacement = 'name="${name} (${tfm})" fullname="${fullname} (${fw})"'
 (cat $logFilePath) -Replace $matchPattern, $replacement | Out-File -Encoding UTF8 $logFilePath
 
 # Run .Net Core tests
-$testDlls = ls -r '.artifacts\*\Debug\*.Tests.dll' | ? FullName -notmatch '\\net\d+\\' `
-  | % FullName | sort-object -Unique
-dotnet vstest $testDlls --results-directory '.\.artifacts\' --logger 'trx;LogFileName=nunit-netcore-results.xml'
+## Emits multiple test reports
+dotnet test .\CodeJam.Light.sln -p:NetCoreTests=true --no-build --no-restore `
+  --results-directory '.\.artifacts\' `
+  --logger 'trx;LogFilePrefix=nunit'
 
-### TODO: replace assembly name in TRX files
+## Update test reports: add TFM to the assemply name
+$matchPattern1 = 'storage="(?''storage''.*?\\(?''tfm''net[^\\]*)\\[^\\]*?\.dll)"'
+$replacement1 = 'storage="${storage} (${tfm}).dll"'
+$matchPattern2 = 'codeBase="(?''codeBase''.*?\\(?''tfm''net[^\\]*)\\[^\\]*?\.dll)"'
+$replacement2 = 'codeBase="${codeBase} (${tfm}).dll"'
+$netCoreReports = ls '.artifacts\nunit_*.trx' | % FullName
+$netCoreReports | ForEach-Object {
+  (cat $_) -Replace $matchPattern1, $replacement1  -Replace $matchPattern2, $replacement2 | Out-File -Encoding UTF8 $_
+}
 
 # Upload files
 $wc = New-Object 'System.Net.WebClient'
-$testResults = ls *.artifacts\nunit-*.xml | % FullName | sort-object -Unique
-foreach ($testResult in $testResults) {
-  echo "UploadFile: https://ci.appveyor.com/api/testresults/nunit3/$env:APPVEYOR_JOB_ID from $testResult"
-  $wc.UploadFile("https://ci.appveyor.com/api/testresults/nunit3/$($env:APPVEYOR_JOB_ID)", $testResult)  
+
+$testResults = ls '.artifacts\nunit_*.xml' | % FullName
+$testResults += ls '.artifacts\nunit_*.trx' | % FullName
+$testResults | ForEach-Object {
+  echo "UploadFile: https://ci.appveyor.com/api/testresults/nunit3/$env:APPVEYOR_JOB_ID from $_"
+  $wc.UploadFile("https://ci.appveyor.com/api/testresults/nunit3/$($env:APPVEYOR_JOB_ID)", $_)
 }
